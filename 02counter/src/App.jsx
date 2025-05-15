24b7bdbeca51328f97aa0a6dbb266c6c571bc3c4
Tirth Panchori <panchoritirth483@gmail.com>
2025-05-16 00:24:59 +0530

    made a counter project using hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15);
+
+  const addvalue = () => {
+    //console.log("Value added,",counter);
+    if (counter < 20) {
+      setCounter(counter+ 1);
+    }
+    
+  }
+
+  const removevalue = () => {
+    //console.log("Value removed, ",counter);
+    if (counter > 0)
+    setCounter(counter - 1);
+  }
+  return (
+    <>
+      
+      <h1>A simple counter program</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick = {addvalue}> Increment value</button>
+      <br />
+      <button onClick = {removevalue}>Decrement value</button>
+    </>
+  )
+}
+
+export default App
